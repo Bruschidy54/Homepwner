bd918d5565df1dc6a6b65cd417eb329a60e0cb16
Dylan Bruschi <dylanbruschi54@gmail.com>
2017-02-24 13:18:45 -0600

    Custom camera overlay and refactoring/organization

---
+++ Homepwner/CustomOverlayView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomOverlayView.swift
+//  Homepwner
+//
+//  Created by Dylan Bruschi on 2/24/17.
+//  Copyright © 2017 Dylan Bruschi. All rights reserved.
+//
+
+import UIKit
+
+class CustomOverlayView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
